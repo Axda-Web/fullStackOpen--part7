d80b768264e128ee585719d54a23f44e54cc3a87
Alyx DARENNE <axda.web@gmail.com>
2023-05-20 17:01:54 +0200

    part7/ex10: Refactor notification state with Redux

--- extending-bloglist/bloglist-frontend/src/components/Notification.jsx
+++ extending-bloglist/bloglist-frontend/src/components/Notification.jsx
@@ -1,8 +1,21 @@
-const Notification = ({ message, classes }) => {
-    if (message === null) {
+import { useSelector } from 'react-redux';
+import { selectNotification } from '../reducers/notificationReducer';
+
+const Notification = () => {
+    const { content, type } = useSelector(selectNotification);
+
+    if (content === null) {
         return null;
     }
-    return <div className={classes}>{message}</div>;
+    return (
+        <div
+            className={`notification notification--${
+                type === 'success' ? 'success' : 'error'
+            }`}
+        >
+            {content}
+        </div>
+    );
 };
 
 export default Notification;
